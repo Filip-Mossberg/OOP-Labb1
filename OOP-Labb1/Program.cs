f15694ab0ef2db23d9a6aad291f98faa04f7ee41
Filip Mossberg <filip.mossberg@gmail.com>
2022-10-27 15:18:43 +0200

    Add project files.

---
+++ OOP-Labb1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace OOP_Labb1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(5);
+            Console.WriteLine($"The Circle Area For {circle.Radie} Is {circle.getArea()}\n");
+
+            Circle circle2 = new Circle(6);
+            Console.WriteLine($"The Circle Area For {circle2.Radie} Is {circle2.getArea()}");
+
+            Console.ReadKey();
+        }
+    }
+
+    internal class Circle
+    {
+        public float Pi = 3.141f;
+        public double Radie;
+
+        public Circle(double radie)
+        {
+            this.Radie = radie;
+        }
+
+        public double getArea()
+        {
+            return (Radie * Radie * Pi);
+        }
+    }
+}
